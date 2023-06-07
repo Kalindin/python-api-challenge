b07153201b6e5123370b90a0e0e7b882aa326059
Kalindin <121258548+Kalindin@users.noreply.github.com>
2023-06-07 19:14:52 -0400

    1

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5c62a522e4529570c4ab3379833fa710"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "41f2f790a0424d02a6538ec624f3f3ec"
