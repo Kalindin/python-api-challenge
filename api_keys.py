12eebac9489893b0c41a55cb18fef83125fff0e5
Evan Hollinshead <121258548+Kalindin@users.noreply.github.com>
2023-06-13 19:56:48 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,8 +1,8 @@
 # OpenWeatherMap API Key
-weather_api_key = "5c62a522e4529570c4ab3379833fa710"
+weather_api_key = 
 
 # Geoapify API Key
-geoapify_key = "41f2f790a0424d02a6538ec624f3f3ec"
+geoapify_key =
 
 
 
